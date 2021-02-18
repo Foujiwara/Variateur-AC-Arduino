47a1a273d17bf5a60c1021fdc1de16a45b489354
Raphaël <leraphael2600@hotmail.fr>
2021-02-18 22:28:54 +0100

    ajust some settings

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,16 @@
 #define TRIAC 6
 //#define ANALOG_IN A0
 #define ThermistorPin A1
-#define DEBUG 1
+#define DEBUG 0
 #define TEMP 1
 #define POT 0
-#define DELAY0 10 //On state Triac delay 
+#define DELAY0 10          //On state Triac delay
 #define AC_FREQUENCY 10000 // 10000 for 50hz, 8333 for 60hz
+#define TEMP_MIN 35        // Température de démarrage du Ventilateur en °C
+#define TEMP_MAX 60        // Température de vitesse Maximum du ventilateur en °C
 
 // ########################## INT ##########################
-uint8_t speed_val = 0;
+int speed_val = 0;
 uint8_t start = 0;
 
 //########################## TEMP ##########################
@@ -25,13 +27,13 @@ float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
 int fonction123()
 {
-  #if TEMP
-  speed_val = map(Tc, 35, 50, 35, 1); // varie de 13 a 30
-  #endif
+#if TEMP
+  speed_val = map(Tc, TEMP_MIN, TEMP_MAX, 35, 1); // varie de 13 a 30
+#endif
 
-  #if POT
+#if POT
   speed_val = map(analogRead(A0), 0, 1023, 1, 35); // varie de 13 a 30
-  #endif
+#endif
   if (speed_val > 30)
   {
     start = 0;
@@ -45,7 +47,7 @@ int fonction123()
     start = 2;
   }
 
-  return(speed_val);
+  return (speed_val);
 }
 
 void zero_crossing()
@@ -97,9 +99,10 @@ void loop()
 #endif
 
 #if DEBUG
-  //Serial.print("Value = " + (speed_val));
-  //Serial.print(" Temperature: ");
-  Serial.println(Tc);
-  //Serial.println(" C");
+  Serial.print(" Temperature= ");
+  Serial.print(Tc);
+  Serial.print(" ADC= ");
+  Serial.println(analogRead(ThermistorPin));
+
 #endif
 }
